cf4f7dde5f5c02101d52c11fec5a2d6cff845ae3
Rahmat Rians <rahmatriansyah57@gmail.com>
2021-03-29 00:01:42 +0700

    final

--- app/src/main/java/com/example/swipeandtabs/MainActivity.java
+++ app/src/main/java/com/example/swipeandtabs/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.swipeandtabs;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
@@ -16,7 +21,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        maTab = (TabLayout)findViewById(R.id.myTab);
+        maTab = (TabLayout)findViewById(R.id.myPager);
         maPage = (ViewPager) findViewById(R.id.myPager);
+
+        maPage.setAdapter(new MyOwnPagerAdapter(getSupportFragmentManager()));
+        maTab.setupWithViewPager(maPage);
+
+        maTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                maPage.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    class MyOwnPagerAdapter extends FragmentPagerAdapter{
+
+        String data[] = {"Java", "Android", "IOS"};
+
+        public MyOwnPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            if(position == 0) {
+                return new java();
+            }
+
+            if(position == 1){
+                return new Android();
+            }
+
+            if(position == 2) {
+                return new IOS();
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return data.length;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return data[position];
+        }
     }
 }
